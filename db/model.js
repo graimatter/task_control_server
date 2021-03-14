2056e7b6cb84d071bdcc5df82c6e6859481acfed
unknown <test@test.com>
2021-03-14 21:20:53 +0700

    init

---
+++ db/model.js
@@ -0,0 +1,177 @@
+
+const sqlite3 = require('sqlite3').verbose();
+const dbName = 'servicedesk.sqlite';
+const db = new sqlite3.Database(__dirname + dbName);
+db.serialize(() => {
+    const createTemplate = 'CREATE TABLE IF NOT EXISTS TASKS_TEMPLATES (TEMPLATE_ID integer primary key, TEMPLATE_TITLE text, ACTIVE integer)';
+    const createTasks = 'CREATE TABLE IF NOT EXISTS TASKS (ID integer primary key, TEMPLATE_ID integer, STATUS integer, DESCRIPTION text)';
+    const createTimes = 'CREATE TABLE IF NOT EXISTS TIMES (ID integer primary key, TASKID integer, DATESTART text, DATEEND text)';
+    db.run(createTemplate);
+    db.run(createTasks);
+    db.run(createTimes);
+
+});
+
+const getRecord = (sql, data) => {
+    return new Promise((resolve, reject) => {
+        db.get(sql, ...data, function (err, row) {
+            if (err)
+                reject(err.message)
+            else{
+                resolve(row)
+            }
+
+        })
+    })
+}
+
+const updateById = (sql, data) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, ...data, function (err, change) {
+            if (err)
+                reject(err.message)
+            else
+                resolve(this.changes)
+
+        })
+    })
+}
+
+const runSQL = (sql) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, function (err) {
+            if (err)
+                reject(err.message)
+            else
+                resolve(this.changes)
+
+        })
+    })
+}
+
+const createNewRecord = (sql, data) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, ...data, function (err) {
+            if (err)
+                reject(err.message)
+            else
+                resolve(this.lastID)
+
+        })
+    })
+}
+
+const selectAll = (sql, data) => {
+    //console.log(`1 ${data}`)
+    return new Promise((resolve, reject) => {
+        db.all(sql, ...data, (err, rows) => {
+            if (err)
+                reject(err.message)
+            else{
+                //rows.forEach(item => console.log(item))
+                resolve(rows)
+            }
+        })
+    })
+}
+
+class Template {
+
+    static create(data) {
+        const sql = `insert into TASKS_TEMPLATES (TEMPLATE_TITLE, ACTIVE) values (?, 1)`
+        return createNewRecord(sql, data)
+    }
+
+    static getAllTemplates() {
+        const sql = `select TEMPLATE_ID as id, TEMPLATE_TITLE as title from TASKS_TEMPLATES where ACTIVE = 1`
+        return selectAll(sql, [])
+    }
+
+    static disactivateTask(data) {
+        const sql = `update TASKS_TEMPLATES set ACTIVE = 0 where TEMPLATE_ID = ?`
+        return updateById(sql, data)
+    }
+
+}
+
+
+class Tasks {
+
+    static create(data) {
+        const sql = `insert into TASKS (TEMPLATE_ID, STATUS, DESCRIPTION) values (?, 2, '')`
+        return createNewRecord(sql, data)
+    }
+
+    static getAllTasks(data) {
+        const sql = `   select  t1.ID as taskId,
+                                t2.TEMPLATE_TITLE as title,
+                                strftime('%H:%M:%S', t3.DATESTART) as time_start,
+                                sum((strftime('%s',t3.DATEEND) - strftime('%s',t3.DATESTART))) as duration,
+                                t1.STATUS as status,
+                                t1.DESCRIPTION as description 
+                        from TASKS as t1
+                        join TASKS_TEMPLATES as t2 on t1.TEMPLATE_ID = t2.TEMPLATE_ID
+                        join TIMES as t3 on t3.TASKID = t1.ID
+                        where t2.ACTIVE = 1 and date(t3.DATESTART) = date(?)
+                        group by t1.ID, t2.TEMPLATE_ID, t2.TEMPLATE_TITLE
+                        ORDER BY t1.STATUS desc, t3.DATESTART`
+        return selectAll(sql, data)
+    }
+
+    static pauseById(data) {
+        const sql = `update TASKS set STATUS = 1 where ID = ?`
+        return updateById(sql, data)
+    }
+
+    static playById(data) {
+        const sql = `update TASKS set STATUS = 2 where ID = ?`
+        return updateById(sql, data)
+    }
+
+    static getCurrectActiveTask() {
+        const sql = 'select ID from TASKS where STATUS = 2'
+        return getRecord(sql, [])
+    }
+
+    static pauseCurrentAcive() {
+        const sql = `update TASKS set STATUS = 1 where STATUS = 2`
+        return runSQL(sql)
+    }
+
+    static closeById(data) {
+        const sql = `update TASKS set STATUS = 0 where ID = ?`
+        return updateById(sql, data)
+    }
+
+    static saveDescription(data) {
+        const sql = `update TASKS set DESCRIPTION = ? where ID = ?`
+        return updateById(sql, data)
+    }
+
+}
+
+class Times {
+
+    static create(data){
+        const sql = `INSERT INTO TIMES (TASKID, DATESTART) VALUES (?,?)`
+        return createNewRecord(sql, data)
+    }
+
+    static close(data) {
+        const sql = `update TIMES set DATEEND = ? where DATEEND is null`
+        return updateById(sql, data)
+    }
+
+    static getTaskDuration(data){
+        const sql = `select sum((strftime('%s',DATEEND) - strftime('%s',DATESTART))) as duration 
+                    from TIMES 
+                    where TASKID = ?`
+        return getRecord(sql, data)
+    }
+
+}
+
+module.exports = db;
+module.exports.Template = Template;
+module.exports.Tasks = Tasks;
+module.exports.Times = Times;
---
+++ routes/index.js
@@ -0,0 +1,227 @@
+var express = require('express');
+var router = express.Router();
+const Template = require('..\\db\\model.js').Template;
+const Tasks = require('..\\db\\model.js').Tasks;
+const Times = require('..\\db\\model.js').Times;
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/createTemplate', function (req, res, next) {
+
+  const newTemplate = req.body
+  try {
+    (async () => {
+      try {
+        let templateId = await (Template.create([newTemplate.title]))
+        res.json({
+          status: 0,
+          result: templateId
+        })
+      }
+      catch (err) {
+        console.log(`**** ${err} ****`)
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+router.post('/deleteTemplate', function (req, res, next) {
+
+  const templateId = req.body.id
+
+  try {
+    (async () => {
+      try {
+        let changes = await (Template.disactivateTask([templateId]))
+        res.json({
+          status: 0,
+          result: changes
+        })
+      }
+      catch (err) {
+        console.log(`**** ${err} ****`)
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+router.post('/getAllTemplates', function (req, res, next) {
+  try {
+    (async () => {
+      try {
+        let tasks = await (Template.getAllTemplates())
+        //console.log(tasks)
+        res.json({
+          status: 0,
+          result: tasks
+        })
+      }
+      catch (err) {
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+router.post('/getAllTasks', function (req, res, next) {
+
+  const curDate = req.body.curDate
+  try {
+    (async () => {
+      try {
+        let tasks = await (Tasks.getAllTasks([curDate]))
+        res.json({
+          status: 0,
+          result: tasks
+        })
+      }
+      catch (err) {
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+router.post('/createNewTask', function (req, res, next) {
+
+  const newTask = req.body
+  let returnRec = {}
+  try {
+    (async () => {
+      try {
+        await (Times.close([newTask.datestart]))
+        let pausedTask = await (Tasks.getCurrectActiveTask())
+        await (Tasks.pauseCurrentAcive())
+        if (pausedTask !== undefined)
+          returnRec = await (Times.getTaskDuration([pausedTask.ID]))
+        let taskId = await (Tasks.create([newTask.id]))
+        await (Times.create([taskId, newTask.datestart]))
+        res.json({
+          status: 0,
+          result: taskId,
+          duration: returnRec.duration
+        })
+      }
+      catch (err) {
+        console.log(`**** ${err} ****`)
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+router.post('/controllTask', function (req, res, next) {
+
+  const currentTask = req.body
+  //console.log(currentTask)
+  //console.log(currentTask)
+  try {
+    (async () => {
+      try {
+        let changedTaskId = -1
+        let returnRec = {}
+        switch (currentTask.action) {
+          case 'pause':
+            changedTaskId = await (Tasks.pauseById([currentTask.id]))
+            await (Times.close([currentTask.date]))
+            returnRec = await (Times.getTaskDuration([currentTask.id]))
+            break
+          case 'play':
+            // set endtime for preveus task
+            await (Times.close([currentTask.date]))
+            // get prevues active task and pause it
+            let preveusTask = await (Tasks.getCurrectActiveTask())
+            await (Tasks.pauseCurrentAcive())
+            // get duration of preveus task
+            if (preveusTask !== undefined)
+              returnRec = await (Times.getTaskDuration([preveusTask.ID]))
+            // start cur task
+            changedTaskId = await (Tasks.playById([currentTask.id]))
+            // set time start for current task
+            await (Times.create([currentTask.id, currentTask.date]))
+            break
+          case 'close':
+            changedTaskId = await (Tasks.closeById([currentTask.id]))
+            await (Times.close([currentTask.date]))
+            returnRec = await (Times.getTaskDuration([currentTask.id]))
+            break
+          case 'save':
+            //console.log(`id = ${currentTask.id} desc = ${currentTask.description}`)
+            returnRec = await (Tasks.saveDescription([currentTask.description, currentTask.id]))
+            break
+        }
+
+        console.log(returnRec.duration)
+        res.json({
+          status: 0,
+          result: changedTaskId,
+          duration: returnRec.duration
+        })
+      }
+      catch (err) {
+        console.log(`**** ${err} ****`)
+        res.json({
+          status: -1,
+          result: err
+        })
+      }
+    })()
+  }
+  catch {
+    res.json({
+      status: -1,
+      result: 'server error'
+    })
+  }
+});
+
+module.exports = router;
